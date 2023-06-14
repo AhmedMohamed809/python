558ef89babf33a2e268d51bcb04f44cf5c366d6a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-14 03:01:53 +0100

    temperaure app

---
+++ temperature.py
@@ -0,0 +1,9 @@
+temp=int(input("Enter the Temperature today ?"))
+
+if temp > 30:
+    print("OH it's a hot day !")
+elif temp < 10 :
+    print("OH it's a cold day ! ")
+elif temp >10:
+    print("OH it's cool day ! ")
+print("Done ")
