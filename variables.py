2dcd011028bbcd38f5bed1e21ed4c937e033affa
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-14 02:44:56 +0100

    string functions

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
-first_name=input("What is your name? ")
-age = input("How old are you? ") 
-is_new_patient=True 
-print("Hello MR "+first_name+" you are  "+age+" years old ! ")
+# first_name=input("What is your name? ")
+# age = input("How old are you? ") 
+# is_new_patient=True 
+# print("Hello MR "+first_name+" you are  "+age+" years old ! ")
+########
+world="ILove learning python"
+print(world.replace("e","x"))
+print(world.find('e'))
+print("python" in world)
