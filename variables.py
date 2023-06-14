84b2aa6dc6258cc51cf8cb82286385e12228b176
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-14 02:11:52 +0100

    use input

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-first_name="Ahmed"
-age = 28 
+first_name=input("What is your name? ")
+age = input("How old are you? ") 
 is_new_patient=True 
-print(first_name,age,is_new_patient)
+print("Hello MR "+first_name+" you are  "+age+" years old ! ")
