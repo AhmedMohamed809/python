2d0e057f307319d0569452bb5cde68780e9b63d8
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-15 04:03:04 +0100

    function

---
+++ function.py
@@ -0,0 +1,7 @@
+def add_tow_number(num1,num2):
+    sum_numbers=int(num1)+int(num2)
+    print("Sum = " + str(sum_numbers))
+    
+num1=input("enter your firest number? ")
+num2=input("enter your secound number? ")
+add_tow_number(num1,num2)
