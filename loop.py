a38f674af485207ee418f26b76ea4c3c88145908
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-15 02:57:12 +0100

    list

--- loop.py
+++ loop.py
@@ -2,3 +2,12 @@ i=1
 while i <=5:
     print(i)
     i=i+1
+
+### list
+names=["Ahmed","Alaa","Hala","Ali"]
+names.insert(0,"Hashim")
+names.append("kmona")
+names.remove("Alaa")
+print(names)  
+print("Ahmed" in names)
+print(len(names))
