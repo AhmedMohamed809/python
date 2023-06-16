0fc6780b770b838ac4f619ae4e5075a91dd75396
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-16 02:15:27 +0100

    filter the large number

--- count.py
+++ count.py
@@ -3,7 +3,8 @@ result = 0
 numbers=[1,2,3,4,5,6]
 for i in numbers:
     result = result + i
-    print(result)
+    if i > 4 :
+        print("large numbers is :"+str(i))
 print("the sum of the arry is : "+str(result))
 average= result / len(numbers)
 print("average is  :" + str(average))
