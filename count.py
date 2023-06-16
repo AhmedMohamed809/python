8c30f5972940e40ffb9f88a3253a066ff01f3a6a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-16 02:12:30 +0100

    get the average

--- count.py
+++ count.py
@@ -1,6 +1,9 @@
 #sum all number in arry 
 result = 0 
-for i in [1,2,3,4,5,6]:
+numbers=[1,2,3,4,5,6]
+for i in numbers:
     result = result + i
     print(result)
 print("the sum of the arry is : "+str(result))
+average= result / len(numbers)
+print("average is  :" + str(average))
