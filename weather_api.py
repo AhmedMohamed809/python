15974b5d95a492822f38f1c17dca1591e7772d35
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-17 23:55:36 +0100

    enter the city name

--- weather_api.py
+++ weather_api.py
@@ -2,3 +2,7 @@ import requests
 
 API_KEY="879c06bd326a87da4ffc25cdacf6a1ea"
 BASE_URL="https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url=f"{BASE_URL}?appid={API_KEY}&q={city}"
+print(request_url)
