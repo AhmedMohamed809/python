0767b2db75204be0253585525a7399c96f882894
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-06-18 00:00:30 +0100

    check the response

--- weather_api.py
+++ weather_api.py
@@ -5,4 +5,12 @@ BASE_URL="https://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
 request_url=f"{BASE_URL}?appid={API_KEY}&q={city}"
-print(request_url)
+response=requests.get(request_url)
+
+if response.status_code == 200 :
+    data = response.json()
+    weather = data['weather']
+  
+ 
+else:
+    print("An error occurred ! ")
